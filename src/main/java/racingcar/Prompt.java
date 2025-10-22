6c05faa8d0487a84168f51b187d509d429c13253
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-10-23 06:00:44 +0900

    feat(Prompt): 프롬프트 메시지 관리 클래스 구현

---
+++ src/main/java/racingcar/Prompt.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import java.util.List;
+
+public class Prompt {
+
+    private Prompt() {
+    }
+
+    private static final String WINNER_PROMPT = "최종 우승자 : ";
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_RACE_ROUNDS = "시도할 횟수는 몇 회인가요?";
+    private static final String CAR_NAME_SEPARATOR = ":";
+    private static final String MOVE_SYMBOL = "-";
+    private static final String WINNER_SEPARATOR = ", ";
+
+
+    public static String buildCarNamesPrompt(){
+        return INPUT_CAR_NAMES;
+    }
+
+    public static String buildRaceRoundsPrompt(){
+        return INPUT_RACE_ROUNDS;
+    }
+
+    public static String buildRacingStatusPrompt(String carName, int moveCount) {
+        return carName + CAR_NAME_SEPARATOR + MOVE_SYMBOL.repeat(moveCount);
+    }
+
+    public static String buildWinnerPrompt(List<String> winners) {
+        return WINNER_PROMPT + String.join(WINNER_SEPARATOR, winners);
+    }
+}
