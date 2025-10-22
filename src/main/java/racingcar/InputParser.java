e6476d599efe5b5e0ae856703de471e26307517e
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-10-23 03:08:23 +0900

    feat(InputParser): 입력 가공 기능 구현

---
+++ src/main/java/racingcar/InputParser.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputParser {
+
+    private static final String DELIMITER = ",";
+
+    private InputParser() {
+    }
+
+    public static List<String> parseCarNames(String input){
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .toList();
+    }
+}
