55dc04e93ffdb6a0fd2f6002a59afcdeedcf4604
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-10-23 05:15:17 +0900

    feat(RandomGenerator): 랜덤값 생성 기능 구현

---
+++ src/main/java/racingcar/RandomGenerator.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomGenerator {
+    private final static int MIN_RANDOM_NUMBER = 1;
+    private final static int MAX_RANDOM_NUMBER = 9;
+
+    public int generateRandomNumber(){
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+    }
+}
