27752b20b3cec03fed0805cbcb41cd891d8550cd
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-10-27 23:24:47 +0900

    refactor(Game): 메서드 분리 및 stream 사용

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -17,13 +17,15 @@ public class Game {
         this.cars = carNames.stream().map(Car::of).toList();
     }
 
-    public void play() {
+    public List<String> play() {
         for (int i = 0; i < raceRounds; i++) {
             for (Car car : cars) {
                 race(car, numberGenerator.generateNumber());
             }
             System.out.println();
         }
+
+        return selectWinners();
     }
 
     private void race(Car car, int randomValue){
@@ -33,14 +35,17 @@ public class Game {
         Output.printRacingStatus(car.getName(), car.getMoveCount());
     }
 
-    public List<String> selectWinners(){
-        int max_value = cars.stream().mapToInt(Car::getMoveCount).max().getAsInt();
-        List<String> winners = new ArrayList<>();
-        for(Car car : cars){
-            if(car.getMoveCount() == max_value){
-                winners.add(car.getName());
-            }
-        }
-        return winners;
+    private List<String> selectWinners(){
+        int max_value = getMaxMoveCount();
+
+        return cars.stream()
+                .filter(car -> car.getMoveCount() == max_value)
+                .map(Car::getName)
+                .toList();
+    }
+
+    private int getMaxMoveCount(){
+        return cars.stream().mapToInt(Car::getMoveCount).max()
+                .orElseThrow(()-> new IllegalArgumentException("최대값이 존재하지 않습니다."));
     }
 }
